654b6d9213c20d698b2560302ea533d08f6ec540
David <davidcarlos.dn@icloud.com>
2025-03-10 22:11:19 -0300

    Initial commit

---
+++ src/test/java/Avanade/_5/Avanade/AvanadeApplicationTests.java
@@ -0,0 +1,13 @@
+package Avanade._5.Avanade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvanadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
